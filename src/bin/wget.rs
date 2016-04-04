ce27764b69617b45e406c42139f9a889545b4f69
Jeremy Soller <jackpot51@gmail.com>
2016-04-04 10:37:06 -0600

    Add wget (WIP)

---
+++ src/bin/wget.rs
@@ -0,0 +1,38 @@
+use std::env;
+use std::fs::File;
+use std::io::{stderr, Read, Write};
+use std::process;
+use std::str;
+
+fn main() {
+    if let Some(url) = env::args().nth(1) {
+        let (scheme, reference) = url.split_at(url.find(':').unwrap_or(0));
+        if scheme == "http" {
+            let mut parts = reference.split('/').skip(2); //skip first two slashes
+            let remote = parts.next().unwrap_or("");
+            let path = parts.next().unwrap_or("/");
+
+            let mut remote_parts = remote.split(':');
+            let host = remote_parts.next().unwrap_or("127.0.0.1");
+            let port = remote_parts.next().unwrap_or("80");
+
+            let tcp = format!("tcp:{}:{}", host, port);
+            write!(stderr(), "{}\n", tcp).unwrap();
+
+            let mut stream = File::open(tcp).unwrap();
+            write!(stream, "GET {} HTTP/1.0\r\n\r\n", path).unwrap();
+            stream.flush().unwrap();
+
+            let mut bytes = [0; 65536];
+            let count = stream.read(&mut bytes).unwrap();
+
+            println!("{}", unsafe { str::from_utf8_unchecked(&bytes[.. count]) });
+        } else {
+            write!(stderr(), "wget: unknown scheme '{}'\n", scheme).unwrap();
+            process::exit(1);
+        }
+    } else {
+        write!(stderr(), "wget: http://host:port/path\n").unwrap();
+        process::exit(1);
+    }
+}
