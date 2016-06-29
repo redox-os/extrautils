ce6c1467f5eccf0fd164a302f36539ad5f821e38
Jeremy Soller <jackpot51@gmail.com>
2016-06-29 16:45:25 -0600

    Use pager in dmesg

---
+++ src/bin/dmesg.rs
@@ -0,0 +1,41 @@
+#![deny(warnings)]
+
+extern crate extra;
+
+use std::env;
+use std::io::{stdout, stderr, Write};
+use std::process::{Command, exit};
+
+use extra::option::OptionalExt;
+
+const MAN_PAGE: &'static str = /* @MANSTART{dmesg} */ r#"
+NAME
+    dmesg - display the system message buffer
+
+SYNOPSIS
+    dmesg [ -h | --help]
+
+DESCRIPTION
+    Displays the contents of the system message buffer.
+
+OPTIONS
+    -h
+    --help
+        display this help and exit
+"#; /* @MANEND */
+
+fn main() {
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    let mut stderr = stderr();
+
+    for arg in env::args().skip(1){
+        if arg.as_str() == "-h" || arg.as_str() == "--help" {
+            stdout.write(MAN_PAGE.as_bytes()).try(&mut stderr);
+            stdout.flush().try(&mut stderr);
+            exit(0);
+        }
+    }
+
+    Command::new("less").arg("syslog:").spawn().unwrap().wait().unwrap();
+}
