728da8d08a57eb3e6db9e2b18c5b8e6a7e664a79
Jeremy Soller <jackpot51@gmail.com>
2017-04-06 21:42:41 -0600

    Add gunzip/gzip

---
+++ src/bin/gunzip.rs
@@ -0,0 +1,48 @@
+#![deny(warnings)]
+
+extern crate extra;
+extern crate libflate;
+
+use extra::option::OptionalExt;
+use libflate::gzip::Decoder;
+use std::{env, fs, io, process};
+use std::io::Write;
+
+fn main() {
+    let mut stderr = io::stderr();
+
+    let mut keep = false;
+    let mut files = Vec::new();
+    for arg in env::args().skip(1) {
+        if arg == "-k" {
+            keep = true;
+        } else {
+            files.push(arg)
+        }
+    }
+
+    if files.is_empty() {
+        writeln!(stderr, "gunzip: no files provided").unwrap();
+        process::exit(1);
+    }
+
+    for arg in files {
+        if arg.ends_with(".gz") {
+            {
+                let input = fs::File::open(&arg).try(&mut stderr);
+                let mut decoder = Decoder::new(input).try(&mut stderr);
+
+                let mut output = fs::File::create(&arg.trim_right_matches(".gz")).try(&mut stderr);
+                io::copy(&mut decoder, &mut output).try(&mut stderr);
+                
+                output.flush().try(&mut stderr);
+            }
+            if ! keep {
+                fs::remove_file(&arg).try(&mut stderr);
+            }
+        } else {
+            writeln!(stderr, "gunzip: {}: unknown suffix", arg).unwrap();
+            process::exit(2);
+        }
+    }
+}
---
+++ src/bin/gzip.rs
@@ -0,0 +1,44 @@
+#![deny(warnings)]
+
+extern crate extra;
+extern crate libflate;
+
+use extra::option::OptionalExt;
+use libflate::gzip::Encoder;
+use std::{env, fs, io, process};
+use std::io::Write;
+
+fn main() {
+    let mut stderr = io::stderr();
+
+    let mut keep = false;
+    let mut files = Vec::new();
+    for arg in env::args().skip(1) {
+        if arg == "-k" {
+            keep = true;
+        } else {
+            files.push(arg)
+        }
+    }
+
+    if files.is_empty() {
+        writeln!(stderr, "gzip: no files provided").unwrap();
+        process::exit(1);
+    }
+
+    for arg in files {
+        {
+            let output = fs::File::create(&format!("{}.gz", &arg)).try(&mut stderr);
+            let mut encoder = Encoder::new(output).try(&mut stderr);
+
+            let mut input = fs::File::open(&arg).try(&mut stderr);
+            io::copy(&mut input, &mut encoder).try(&mut stderr);
+            
+            let mut encoded = encoder.finish().into_result().try(&mut stderr);
+            encoded.flush().try(&mut stderr);
+        }
+        if ! keep {
+            fs::remove_file(&arg).try(&mut stderr);
+        }
+    }
+}
