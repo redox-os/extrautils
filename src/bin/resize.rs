19d3d9ded97ab3fc260d70675a6bce46424f8526
Jeremy Soller <jackpot51@gmail.com>
2017-04-08 17:52:55 -0600

    Fix resize program by using get_tty

--- src/bin/resize.rs
+++ src/bin/resize.rs
@@ -1,25 +1,30 @@
+extern crate extra;
 extern crate termion;
 
+use extra::option::OptionalExt;
 use std::io::{self, Write};
 use termion::cursor::{self, DetectCursorPos};
 use termion::raw::IntoRawMode;
 
 fn main() {
-    let mut stdout = io::stdout().into_raw_mode().unwrap();
+    let mut stderr = io::stderr();
 
-    write!(stdout, "{}", cursor::Save).unwrap();
-    stdout.flush().unwrap();
+    let mut terminal = termion::get_tty().try(&mut stderr)
+                               .into_raw_mode().try(&mut stderr);
 
-    write!(stdout, "{}", cursor::Goto(999, 999)).unwrap();
-    stdout.flush().unwrap();
+    write!(terminal, "{}", cursor::Save).unwrap();
+    terminal.flush().unwrap();
 
-    let (w, h) = stdout.cursor_pos().unwrap();
+    write!(terminal, "{}", cursor::Goto(999, 999)).unwrap();
+    terminal.flush().unwrap();
 
-    write!(stdout, "{}", cursor::Restore).unwrap();
-    stdout.flush().unwrap();
+    let (w, h) = terminal.cursor_pos().unwrap();
 
-    drop(stdout);
+    write!(terminal, "{}", cursor::Restore).unwrap();
+    terminal.flush().unwrap();
+    
+    drop(terminal);
 
-    println!("export COLUMNS={}", w);
-    println!("export LINES={}", h);
+    println!("export COLUMNS={};", w);
+    println!("export LINES={};", h);
 }
