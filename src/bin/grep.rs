6daf55566acf1584264a17cdda3a71fc6b6077be
Chad Williamson <chad@dahc.us>
2020-12-24 00:57:43 -0800

    Implement filename header switches and defaults in grep

--- src/bin/grep.rs
+++ src/bin/grep.rs
@@ -12,28 +12,35 @@ NAME
     grep - print lines matching a pattern
 
 SYNOPSIS
-    grep [-h | --help] [-n --line-number] PATTERN [FILE...]
+    grep [--help] [-chHnv] PATTERN [FILE...]
 
 DESCRIPTION
     grep searches the named input FILEs for lines containing a match to the given PATTERN. If no
     files are specified, grep searches the standard input. grep prints the matching lines.
 
 OPTIONS
-    -h
-    --help
-        Print this manual page.
-
-    -v
-    --invert-match
-        Invert matching.
-
     -c
     --count
         Print count of matching lines, instead of those lines.
 
+    -H
+    --with-filename
+        Include filename header with each match (default for multiple files).
+
+    -h
+    --no-filename
+        Never include filename header (default for single files or stdin).
+
+    --help
+        Print this manual page.
+
     -n
     --line-number
         Prefix each line of output with the line number of the match.
+
+    -v
+    --invert-match
+        Invert matching.
 "#; /* @MANEND */
 
 #[derive(Copy, Clone)]
@@ -41,6 +48,7 @@ struct Flags {
     line_numbers: bool,
     invert_match: bool,
     count: bool,
+    filename_headers: Option<bool>,
 }
 
 impl Flags {
@@ -49,6 +57,7 @@ impl Flags {
             line_numbers: false,
             invert_match: false,
             count: false,
+            filename_headers: None,
         }
     }
 }
@@ -64,10 +73,12 @@ fn main() {
     for arg in args {
         if arg.starts_with('-') {
             match arg.as_str() {
-                "-h" | "--help" => {
+                "--help" => {
                     print!("{}", MAN_PAGE);
                     exit(0);
                 }
+                "-h" | "--no-filename" => flags.filename_headers = Some(false),
+                "-H" | "--with-filename" => flags.filename_headers = Some(true),
                 "-n" | "--line-number" => flags.line_numbers = true,
                 "-v" | "--invert-match" => flags.invert_match = true,
                 "-c" | "--count" => flags.count = true,
@@ -87,16 +98,19 @@ fn main() {
         eprintln!("You must provide a pattern");
         exit(2);
     }
+    if flags.filename_headers == None {
+        flags.filename_headers = Some(files.len() > 1);
+    }
 
     let mut found = false;
     let mut error = false;
     if files.is_empty() {
-        found = do_simple_search(BufReader::new(stdin), &pattern, flags);
+        found = do_simple_search(BufReader::new(stdin), "(standard input)", &pattern, flags);
     } else {
         for path in files {
             match File::open(&Path::new(&path)) {
                 Ok(f) => {
-                    found |= do_simple_search(BufReader::new(f), &pattern, flags);
+                    found |= do_simple_search(BufReader::new(f), &path, &pattern, flags);
                 }
                 Err(err) => {
                     eprintln!("Error opening {}: {}", path, err);
@@ -113,7 +127,7 @@ fn main() {
     }
 }
 
-fn do_simple_search<T: BufRead>(reader: T, pattern: &str, flags: Flags) -> bool {
+fn do_simple_search<T: BufRead>(reader: T, path: &str, pattern: &str, flags: Flags) -> bool {
     let mut count = 0;
     for (line_num, result) in reader.lines().enumerate() {
         if let Ok(line) = result {
@@ -124,8 +138,11 @@ fn do_simple_search<T: BufRead>(reader: T, pattern: &str, flags: Flags) -> bool 
             if is_match {
                 count += 1;
                 if !flags.count {
+                    if flags.filename_headers.unwrap() {
+                        print!("{}:", path);
+                    }
                     if flags.line_numbers {
-                        print!("{}: ", line_num + 1);
+                        print!("{}:", line_num + 1);
                     }
                     println!("{}", line);
                 }
@@ -134,6 +151,9 @@ fn do_simple_search<T: BufRead>(reader: T, pattern: &str, flags: Flags) -> bool 
     }
 
     if flags.count {
+        if flags.filename_headers.unwrap() {
+            print!("{}:", path);
+        }
         println!("{}", count);
     }
 
